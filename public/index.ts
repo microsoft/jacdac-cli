51876e8bc25179c6b88c8360ce5c877f1af1db93
pelikhan <jhalleux@microsoft.com>
2021-12-31 10:21:56 -0800

    fix: add reconnect logic to devtools

--- public/index.ts
+++ public/index.ts
@@ -9,36 +9,49 @@
     const hostname = location.hostname
     const port = secure ? 443 : 8081
     const wsurl = `${protocol}//${hostname}:${port}/`
-    // node.js -> iframe dashboard
-    const ws = new WebSocket(wsurl)
-    ws.binaryType = "arraybuffer"
-    console.debug(`devtools: connecting ${wsurl}...`)
-    ws.addEventListener("open", () => {
-        console.debug(`devtools: connected ${ws.url}`)
-    })
-    ws.addEventListener("message", (msg) => {
-        const data = new Uint8Array(msg.data)
-        const pktMsg = {
-            type: "messagepacket",
-            channel: "jacdac",
-            data,
-            sender,
-        }
-        frame.contentWindow.postMessage(pktMsg, "*")
-    })
-    ws.addEventListener("close", () => {
-        console.debug(`devtools: connection closed`)
-    })
-    ws.addEventListener("error", (e: Event) => {
-        console.error(`devtools: error ${e + ""}`, e)
-    })
-    // iframe dashboard -> node.js
-    window.addEventListener("message", msg => {
-        const data = msg.data
-        if (data && data.type === "messagepacket" && data.channel === "jacdac") {
-            if (ws?.readyState === WebSocket.OPEN) {
-                ws.send(data.data)
+
+    let ws: WebSocket
+    const connectSocket = () => {
+        // already connected or connecting
+        if (ws) return
+
+        // node.js -> iframe dashboard
+        ws = new WebSocket(wsurl)
+        ws.binaryType = "arraybuffer"
+        console.debug(`devtools: connecting ${wsurl}...`)
+        ws.addEventListener("open", () => {
+            console.debug(`devtools: connected ${ws.url}`)
+        })
+        ws.addEventListener("message", (msg) => {
+            const data = new Uint8Array(msg.data)
+            const pktMsg = {
+                type: "messagepacket",
+                channel: "jacdac",
+                data,
+                sender,
             }
-        }
-    })
+            frame.contentWindow.postMessage(pktMsg, "*")
+        })
+        ws.addEventListener("close", () => {
+            console.debug(`devtools: connection closed`)
+            ws = undefined
+        })
+        ws.addEventListener("error", (e: Event) => {
+            console.error(`devtools: error ${e + ""}`, e)
+            ws?.close()
+        })
+        // iframe dashboard -> node.js
+        window.addEventListener("message", msg => {
+            const data = msg.data
+            if (data && data.type === "messagepacket" && data.channel === "jacdac") {
+                if (ws?.readyState === WebSocket.OPEN) {
+                    ws.send(data.data)
+                }
+            }
+        })
+    }
+
+    // start background connection
+    setInterval(connectSocket, 5000);
+    connectSocket();
 })()
